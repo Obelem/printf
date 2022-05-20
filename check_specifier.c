1ebcd64d1b0011e8be8727c4b0d57efbe9471b3a
Franklin Obasi <franklinobasy@gmail.com>
2022-05-20 04:47:22 -0700

    created a file for check_spcifier function - franklin

---
+++ check_specifier.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * check_specifier - checks for specifier
+ * @format: format string
+ * @count: char to be printed
+ * @i: format string iterator
+ * @args: va_list
+ * Return: count
+ */
+
+int check_specifier(const char *format, int count, int i, va_list args)
+{
+	int j = 0;
+	char *str;
+
+	if (format[i + 1] == '%')
+	{
+		_putchar(format[i]);
+		count++;
+		return (count);
+	}
+
+	else if (format[i + 1] == 'c')
+	{
+		_putchar(va_arg(args, int));
+		count++;
+		return (count);
+	}
+
+	else if (format[i + 1] == 's')
+	{
+		str = va_arg(args, char *);
+		if (!str)
+			return (-1);
+		for (j = 0; str[j] != '\0'; j++)
+		{
+			_putchar(str[j]);
+			count++;
+		}
+
+		return (count);
+	}
+
+	else
+	{
+		_putchar('%');
+		_putchar(format[i + 1]);
+		count = count + 2;
+		return (count);
+	}
+	return (-1);
+}
