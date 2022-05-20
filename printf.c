92abd3e0644272bb2b1f9e841723962da70c218a
Braide Jesse <ibbraidejesse@gmail.com>
2022-05-20 12:12:36 -0700

    task 0: %s, %c and %% format for printf

--- printf.c
+++ printf.c
@@ -12,10 +12,10 @@ int _printf(const char *format, ...)
 	va_list args;
 	int i, count = 0;
 
-	va_start(args, format);
-
 	if (format == NULL)
 		return (-1);
+
+	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
