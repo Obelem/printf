978c689c156e0bbbf31c59823cbc9148be9089cd
Obasi Franklin <58641266+franklinobasy@users.noreply.github.com>
2022-05-21 17:03:25 +0100

    Update main.h

--- main.h
+++ main.h
@@ -25,6 +25,7 @@ int print_c(va_list c);
 int print_s(va_list s);
 int print_i(va_list i);
 int print_d(va_list d);
+int print_b(va_list b);
 int (*check_specifier(const char *format))(va_list);
 
 #endif
