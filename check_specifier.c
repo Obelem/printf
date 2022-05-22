53b3e39ce6836d92cce6eb67955d02b30b449d00
Obasi Franklin <58641266+franklinobasy@users.noreply.github.com>
2022-05-22 04:21:49 +0100

    Update check_specifier.c

--- check_specifier.c
+++ check_specifier.c
@@ -18,6 +18,10 @@ int (*check_specifier(const char *format))(va_list args)
 		{"i", print_i},
 		{"d", print_d},
 		{"b", print_b},
+		{"u", print_u},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
 		{NULL, NULL}
 	};
 
